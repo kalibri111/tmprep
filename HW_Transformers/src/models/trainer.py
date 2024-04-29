5c556859dea674481e495824c481877d485949c4
Any <maksimova.am@phystech.edu>
2024-04-29 17:42:28 +0300

    fix

--- HW_Transformers/src/models/trainer.py
+++ HW_Transformers/src/models/trainer.py
@@ -1,5 +1,5 @@
-import torch
-from tqdm.auto import tqdm
+from tqdm import tqdm
+
 
 
 class Trainer:
@@ -12,33 +12,31 @@ class Trainer:
 
     def train(self, train_dataloader, val_dataloader):
         try:
-            for epoch in tqdm(range(self.epoch_num), position=0, leave=False):
+            for epoch in tqdm(range(self.epoch_num)):
                 train_epoch_loss = 0
                 self.model.train()
-                for batch in tqdm(train_dataloader, position=1, leave=False):
+                for batch in train_dataloader:
                     train_loss = self.model.training_step(batch)
                     train_epoch_loss += train_loss
                 train_epoch_loss = train_epoch_loss / len(train_dataloader)
 
                 val_epoch_loss, val_epoch_bleu = 0, 0
                 self.model.eval()
-                for batch in tqdm(val_dataloader, position=1, leave=False):
+                for batch in val_dataloader:
                     val_loss = self.model.validation_step(batch)
                     val_epoch_loss += val_loss
                 val_epoch_loss = val_epoch_loss / len(val_dataloader)
 
                 input_tensor, target_tensor = batch
-                with torch.no_grad():
-                    predicted_samples, _ = self.model.forward(input_tensor)
-                bleu_score, actual_sentences, predicted_sentences = self.model.eval_bleu(predicted_samples,
-                                                                                         target_tensor)
+                predicted_samples, _ = self.model.forward(input_tensor)
+                bleu_score, actual_sentences, predicted_sentences = self.model.eval_bleu(predicted_samples, target_tensor)
                 print('Current BLEU: ', bleu_score)
                 for a, b in zip(actual_sentences[:5], predicted_sentences[:5]):
                     print(f"{a} ---> {b}")
                 print('##############################')
 
                 self.logger.log({"val_loss": val_epoch_loss,
-                                 "train_loss": train_epoch_loss,
+                                 "train_loss": train_epoch_loss ,
                                  "bleu_score": bleu_score})
 
         except KeyboardInterrupt:
